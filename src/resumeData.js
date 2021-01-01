dbe79c8e38528ee073fb788e99b4f0f4f143e555
mkolek1015 <mkolek7@yahoo.com>
2021-01-01 15:05:18 -0500

    FirstRound

---
+++ src/resumeData.js
@@ -0,0 +1,128 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Michael Kolek ",
+    "role": "Graphic Designer and Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mike-kolek-6159ab7/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mkolek1015",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/mkolek7",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a Graphic Artist based out of Manchester, CT. I have spent the last 19 years working in the Graphic Design field whether as a designer, in layout & design, print (catalogs), production and fabrication (sign making, label printing, vehicle lettering). With the recent pandemic of 2020, I decided to take the opportunity to learn a new technology and have since completed a Certification in Full-Stack Web Development from the University of Connecticut & Trilogy, where I learned ideal practices behind the design of mobile and user-friendly websites and applications. I am actively looking for a job in Web-Development where I hope to combine with my Graphic Design experience and merge it into a new full-time career!",
+    "address":"Connecticut, USA",
+    "website":"https://github.com/mkolek1015",
+    "education":[
+      {
+        "UniversityName":"UConn Web Development Bootcamp",
+        "specialization":"",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2021",
+        "Achievements":"Certificate in Full-Stack Web Development"
+      },
+      {
+        "UniversityName":"Endicott College",
+        "specialization":"Graphic Design",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2002",
+        "Achievements":"B.F.A. - Visual Communications"
+      },
+      {
+        "UniversityName":"W3Schools.com",
+        "specialization":"Web Developer onsite Education",
+        "MonthOfPassing":"On",
+        "YearOfPassing":"Going",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Simoniz, USA",
+        "specialization":"Printing Production Operator | Graphic Designer",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"",
+        "Achievements":"Responsible for creating exciting material and design solutions that have a high visual impact for packaging and Point of Sales material for carwash & janitorial cleaning supplies. Maintain active communication with sales representatives from initial concepts to final production. Point of Sale and Marketing materials include advertisements, brochures, catalogs, handouts, and labels."
+      },
+      {
+        "CompanyName":"Central Connecticut State University",
+        "specialization":"Women’s Varsity Volleyball Club Coach",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2015",
+        "Achievements":"Head Coach for a Division 1 Collegiate Club Volleyball team which belongs to the Northeast Women's Volleyball Club League (NWVCL). Competed against other colleges and universities throughout the New England region."
+      }
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Photoshop"
+      },
+      {
+        "skillname":"Illustrator"
+      },
+      {
+        "skillname":"InDesign"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"css"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"Photography"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
